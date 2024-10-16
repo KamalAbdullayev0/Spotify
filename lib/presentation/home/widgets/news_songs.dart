b6e17cf4b477493ca5f605babfaf54cd1c3295b8
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-10-16 12:28:41 +0400

    34

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify/common/helpers/is_dark_mode.dart';
 import 'package:spotify/core/configs/constants/app_urls.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
 import 'package:spotify/domain/entities/song/song.dart';
 import 'package:spotify/presentation/home/bloc/news_songs_cubit.dart';
 import 'package:spotify/presentation/home/bloc/news_songs_state.dart';
@@ -41,13 +43,12 @@ class NewsSongs extends StatelessWidget {
       shrinkWrap: true,
       itemBuilder: (context, index) {
         return SizedBox(
-          width: 100,
+          width: 160,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: Container(
-                  height: 100,
-                  width: 100,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
                     image: DecorationImage(
@@ -57,9 +58,48 @@ class NewsSongs extends StatelessWidget {
                       ),
                     ),
                   ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      transform: Matrix4.translationValues(4, 4, 0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: context.isDarkMode
+                            ? AppColors.darkGrey
+                            : Color(0xffE6E6E6),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.play_arrow_rounded,
+                          color: context.isDarkMode
+                              ? const Color(0xff959595)
+                              : const Color(0xff555555),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
               const SizedBox(height: 8),
+              Text(
+                songs[index].title,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                songs[index].artist,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
             ],
           ),
         );
